d9bedd00c1e8fa2ea79e6474e3b31a7929932c11
GIGIJU-DESKTOP <xenon0625@gmail.com>
2024-07-09 16:12:29 +0900

    First Commit

---
+++ src/main/java/kr/co/ginong/web/GinongApplication.java
@@ -0,0 +1,13 @@
+package kr.co.ginong.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GinongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GinongApplication.class, args);
+    }
+
+}
