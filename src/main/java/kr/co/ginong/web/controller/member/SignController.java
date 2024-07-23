0b3115b9267bb3b7208bfca57888cd0350295a02
JiniBean_Desktop <eunjini6645@gmail.com>
2024-07-23 17:38:38 +0900

    Feat: 회원가입 작업중(step1 완료)
    1. post 메소드 테스트 완료
    2. 유효성 검사 완료

--- src/main/java/kr/co/ginong/web/controller/member/SignController.java
+++ src/main/java/kr/co/ginong/web/controller/member/SignController.java
@@ -1,8 +1,10 @@
 package kr.co.ginong.web.controller.member;
 
+import jakarta.servlet.http.HttpSession;
+import kr.co.ginong.web.entity.member.Mbr;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping()
 @Controller
@@ -14,7 +16,44 @@ public class SignController {
     }
 
     @GetMapping("signup/step1")
-    public String step1() {
+    public String step1(HttpSession session, Model model) {
+        Boolean age = (Boolean) session.getAttribute("age");
+        Boolean agree = (Boolean) session.getAttribute("agree");
+        Boolean email = (Boolean) session.getAttribute("email");
+        Boolean all = age && agree && email;
+
+        model.addAttribute("all", all);
+        model.addAttribute("age", age);
+        model.addAttribute("agree", agree);
+        model.addAttribute("email", email);
         return "member/sign/step1";
+    }
+
+    @PostMapping("signup/step1")
+    public String step1(@RequestParam Boolean age
+                        , @RequestParam Boolean agree
+                        , @RequestParam(defaultValue = "false") Boolean email
+                        , HttpSession session) {
+
+
+        session.setAttribute("age",age);
+        session.setAttribute("agree",agree);
+        session.setAttribute("email",email);
+        return "redirect:step2";
+    }
+
+    @GetMapping("signup/step2")
+    public String step2(){
+        return "member/sign/step2";
+    }
+
+    @PostMapping("signup/step2")
+    public String step2(@RequestParam Mbr member
+            ,HttpSession session) {
+
+        Boolean rxEmail = (Boolean) session.getAttribute("email");
+        member.setEmailRxYn(rxEmail);
+        session.setAttribute("member",member);
+        return "redirect:step3";
     }
 }
